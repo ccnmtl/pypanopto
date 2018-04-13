4f23c811afc277040294b1d385d3b499de8d1d9c
Susan Dreher <sdreher@columbia.edu>
2018-04-13 15:42:34 -0400

    More api integration & disintegration
    * adding & removing integration points
    * @todo add tests

--- panopto/session.py
+++ panopto/session.py
@@ -26,46 +26,6 @@ class PanoptoSessionManager(object):
             server, name)
         return Client(url)
 
-    def add_folder(self, name, parent_guid):
-        try:
-            response = self.client['session'].service.AddFolder(
-                auth=self.auth_info, name=name, parentFolder=parent_guid,
-                isPublic=False)
-
-            if response is None or len(response) < 1:
-                return ''
-
-            obj = serialize_object(response)
-            return obj['Id']
-        except Fault:
-            return ''
-
-    def get_session_url(self, session_id):
-        try:
-            response = self.client['session'].service.GetSessionsById(
-                auth=self.auth_info, sessionIds=[session_id])
-
-            if response is None or len(response) < 1:
-                return ''
-
-            obj = serialize_object(response)
-            return obj[0]['MP4Url']
-        except Fault:
-            return ''
-
-    def get_group_guid(self, group_name):
-        try:
-            response = self.client['user'].service.GetGroupsByName(
-                auth=self.auth_info, groupName=group_name)
-
-            if not response or len(response) < 1:
-                return None
-
-            obj = serialize_object(response)
-            return obj[0]['Id']
-        except Fault:
-            return None
-
     def get_user_guids(self, usernames):
         guids = []
         for user in usernames:
@@ -86,6 +46,61 @@ class PanoptoSessionManager(object):
                 return ''
         return guids
 
+    def add_folder(self, name, parent_guid):
+        try:
+            response = self.client['session'].service.AddFolder(
+                auth=self.auth_info, name=name, parentFolder=parent_guid,
+                isPublic=False)
+
+            if response is None or len(response) < 1:
+                return ''
+
+            obj = serialize_object(response)
+            return obj['Id']
+        except Fault:
+            return ''
+
+    def grant_group_folder_access_by_guid(self, folder, group):
+        try:
+            api = self.client['access']
+            response = api.service.GrantGroupAccessToFolder(
+                auth=self.auth_info, folderId=folder, groupId=group,
+                role='Viewer')
+
+            if response is None or len(response) < 1:
+                return False
+
+            return True
+        except Fault:
+            return False
+
+    def revoke_group_folder_access_by_guid(self, folder, group):
+        try:
+            api = self.client['access']
+            response = api.service.RevokeGroupAccessFromFolder(
+                auth=self.auth_info, folderId=folder, groupId=group,
+                role='Viewer')
+
+            if response is None or len(response) < 1:
+                return False
+
+            return True
+        except Fault:
+            return False
+
+    def get_session_url(self, session_id):
+        try:
+            response = self.client['session'].service.GetSessionsById(
+                auth=self.auth_info, sessionIds=[session_id])
+
+            if response is None or len(response) < 1:
+                return ''
+
+            obj = serialize_object(response)
+            return obj[0]['MP4Url']
+        except Fault:
+            return ''
+
     def grant_users_viewer_access(self, session_id, usernames):
         '''
             Update a session's owner, can only be called by an admin
@@ -95,24 +110,6 @@ class PanoptoSessionManager(object):
             guids = self.get_user_guids(usernames)
             api = self.client['access']
             response = api.service.GrantUsersViewerAccessToSession(
-                auth=self.auth_info, sessionId=session_id, userIds=guids)
-
-            if response is None or len(response) < 1:
-                return False
-
-            return True
-        except Fault:
-            return False
-
-    def grant_group_viewer_access(self, session_id, group_name):
-        '''
-            Update a session's owner, can only be called by an admin
-            or the creator.
-        '''
-        try:
-            guids = self.get_group_guid(group_name)
-            api = self.client['access']
-            response = api.service.GrantGroupViewerAccessToSession(
                 auth=self.auth_info, sessionId=session_id, userIds=guids)
 
             if response is None or len(response) < 1:
