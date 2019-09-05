aacf3e4702b6f6a8959a42d1277f04c9278004ee
Susan Dreher <sdreher@gmail.com>
2019-09-05 15:53:52 -0400

    Prep release 0.1.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="pypanopto",
-    version="0.0.9",
+    version="0.1.0",
     author="Susan Dreher",
     author_email="sdreher@columbia.edu",
     url="https://github.com/ccnmtl/pypanopto",
